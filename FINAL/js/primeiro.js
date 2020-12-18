50e559eb10580a56711d84219c158b6e0d7e0daf
Renan Groth <31290593+renangroth@users.noreply.github.com>
2020-12-18 14:08:32 -0300

    Versão 3
    Adicionado segunda luz, arvore de natal, presentes embaixo da arvore e 2 alvos

--- FINAL/js/primeiro.js
+++ FINAL/js/primeiro.js
@@ -1,21 +1,66 @@
-var scene, camera, renderer, mesh, clock;
+var scene, camera, renderer, mesh;
 var meshFloor, ambientLight, light;
-
-var crate, crateTexture, crateNormalMap, crateBumpMap;
 
 var keyboard = {};
 var player = { height:1.8, speed:0.2, canShoot:0 };
-var USE_WIREFRAME = false;
 
 var loadingManager = null;
 var RESOURCES_LOADED = false;
 
 var models = { //  index de modelos
 	machine: {
-		obj:"assets/models/machinegun.obj",
-		mtl:"assets/models/machinegun.mtl",
+		obj:"assets/models/m4.obj",
+		mtl:"assets/models/m4.mtl",
 		mesh: null,
 		castShadow:false
+	},
+
+	arvoreNatal: {
+		obj:"assets/models/arvoreNatal.obj",
+		mtl:"assets/models/arvoreNatal.mtl",
+		mesh: null
+	},
+
+	presente1: {
+		obj:"assets/models/presente1.obj",
+		mtl:"assets/models/presente1.mtl",
+		mesh: null
+	}, 
+
+	presente2: {
+		obj:"assets/models/presente2.obj",
+		mtl:"assets/models/presente2.mtl",
+		mesh: null
+	}, 
+
+	presente3: {
+		obj:"assets/models/presente3.obj",
+		mtl:"assets/models/presente3.mtl",
+		mesh: null
+	}, 
+
+	presente4: {
+		obj:"assets/models/presente4.obj",
+		mtl:"assets/models/presente4.mtl",
+		mesh: null
+	}, 
+
+	presente5: {
+		obj:"assets/models/presente5.obj",
+		mtl:"assets/models/presente5.mtl",
+		mesh: null
+	},
+
+	Alvo1: {
+		obj:"assets/models/Alvo1.obj",
+		mtl:"assets/models/Alvo1.mtl",
+		mesh: null
+	}, 
+
+	Alvo2: {
+		obj:"assets/models/Alvo2.obj",
+		mtl:"assets/models/Alvo2.mtl",
+		mesh: null
 	}
 };
 
@@ -36,14 +81,11 @@ function init(){
 		onResourcesLoaded();
 	};
 	
-	mesh = new THREE.Mesh(
-		new THREE.BoxGeometry(1,1,1),
-		new THREE.MeshPhongMaterial({color:0xff4444, wireframe:USE_WIREFRAME})
-	);
+	mesh = new THREE.Mesh();
 	
 	meshFloor = new THREE.Mesh(
-		new THREE.PlaneGeometry(60,60, 30,30),
-		new THREE.MeshPhongMaterial({color:0x3D85C6, wireframe:USE_WIREFRAME})
+		new THREE.PlaneGeometry(30,30, 30,30),
+		new THREE.MeshPhongMaterial({color:0x3D85C6})
 	);
 	meshFloor.rotation.x -= Math.PI / 2;
 	meshFloor.receiveShadow = true;
@@ -60,18 +102,16 @@ function init(){
 	scene.add(light);
 	
 	light2 = new THREE.PointLight(0xffffff, 0.8, 18); // Poste
-	light2.position.set(25,6,10);
+	light2.position.set(12,7,10);
 	light2.castShadow = true;
 	light2.shadow.camera.near = 0.1;
 	light2.shadow.camera.far = 25;
 	scene.add(light2);
 
-	var textureLoader = new THREE.TextureLoader(loadingManager);
 	for( var _key in models ){
 		(function(key){
 			var mtlLoader = new THREE.MTLLoader(loadingManager);
-			mtlLoader.load(models[key].mtl, function(materials){
-				materials.preload();				
+			mtlLoader.load(models[key].mtl, function(materials){			
 				var objLoader = new THREE.OBJLoader(loadingManager);
 				objLoader.setMaterials(materials);
 				objLoader.load(models[key].obj, function(mesh){
@@ -104,59 +144,90 @@ function init(){
 	renderer.shadowMap.type = THREE.BasicShadowMap;
 	
 	document.body.appendChild(renderer.domElement);
-	
-	animate();
+	animate();	
 }
 
 // É executado quando todos os recursos são carregados
-function onResourcesLoaded(){// Reposiciona malhas individuais e, em seguida, adicione as malhas à cena
-	meshes["playerweapon"] = models.machine.mesh.clone(); //  arma do jogador
+function onResourcesLoaded(){//Malhas individuais
+	meshes["playerweapon"] = models.machine.mesh.clone(); //arma do jogador
 	meshes["playerweapon"].position.set(0,2,0);
-	meshes["playerweapon"].scale.set(10,10,10);
+	meshes["playerweapon"].scale.set(0.8,0.8,0.8);
 	scene.add(meshes["playerweapon"]);
+
+	meshes["arvoreNatal"] = models.arvoreNatal.mesh.clone();
+	meshes["arvoreNatal"].position.set(12,0,10);
+	meshes["arvoreNatal"].scale.set(3,3,3);
+	scene.add(meshes["arvoreNatal"]);
+
+	meshes["presente1"] = models.presente1.mesh.clone();
+	meshes["presente1"].position.set(11,0,10);
+	scene.add(meshes["presente1"]);
+
+	meshes["presente2"] = models.presente2.mesh.clone();
+	meshes["presente2"].position.set(13,0,11);
+	scene.add(meshes["presente2"]);
+
+	meshes["presente3"] = models.presente3.mesh.clone();
+	meshes["presente3"].position.set(13,0,9);
+	scene.add(meshes["presente3"]);
+
+	meshes["presente4"] = models.presente4.mesh.clone();
+	meshes["presente4"].position.set(12,0,9);
+	scene.add(meshes["presente4"]);
+
+	meshes["presente5"] = models.presente5.mesh.clone();
+	meshes["presente5"].position.set(12,0,11);
+	scene.add(meshes["presente5"]);
+
+	meshes["Alvo1"] = models.Alvo1.mesh.clone();
+	meshes["Alvo1"].position.set(-15,1.4,11);
+	meshes["Alvo1"].rotation.y -= 3;
+	meshes["Alvo1"].scale.set(5,5,5);
+	scene.add(meshes["Alvo1"]);
+
+	meshes["Alvo2"] = models.Alvo2.mesh.clone();
+	meshes["Alvo2"].position.set(-11,1.4,15);
+	meshes["Alvo2"].rotation.y -= 1.7;
+	meshes["Alvo2"].scale.set(5,5,5);
+	scene.add(meshes["Alvo2"]);
 }
 
 function animate(){
-	// Joga a tela de carregamento até que os recursos sejam carregados
-	if( RESOURCES_LOADED == false ){requestAnimationFrame(animate);return;}
-
 	requestAnimationFrame(animate);
 	
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;	
+	mesh.rotation.x += 0.0001;
+	mesh.rotation.y += 0.0001;	
 	
-	// ir até a matriz de balas e atualizar a posição
-	// remove balas quando apropriado
+	// Atualiza posicao das balas
 	for(var index=0; index<bullets.length; index+=1){
 		if( bullets[index] === undefined ) continue;
 		if( bullets[index].alive == false ){
 			bullets.splice(index,1);
 			continue;
 		}
-		
 		bullets[index].position.add(bullets[index].velocity);
 	}
 	
-	if(keyboard[87]){ // W key
+	if(keyboard[87] ){ // W
 		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
 		camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
 	}
-	if(keyboard[83]){ // S key
+	if(keyboard[83]){ // S
 		camera.position.x += Math.sin(camera.rotation.y) * player.speed;
 		camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
 	}
-	if(keyboard[65]){ // A key
+	if(keyboard[65]){ // A
 		camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
 		camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
 	}
-	if(keyboard[68]){ // D key
+	if(keyboard[68]){ // D
 		camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
 		camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
 	}
 	
-	if (cliquePressionado && player.canShoot <= 0){ // joga a bala
-		var bullet = new THREE.Mesh( // cria a bala como um Mesh objeto
-			new THREE.SphereGeometry(0.05,8,8),
+	if (cliquePressionado && player.canShoot <= 0){ // Atira a bala
+		var bullet = new THREE.Mesh( // cria a bala
+			new THREE.SphereGeometry(0.03,6,6),
 			new THREE.MeshBasicMaterial({color:0xffffff})
 		);	
 		
@@ -172,16 +243,14 @@ function animate(){
 			Math.cos(camera.rotation.y)
 		);
 		
-		// após 1000 ms, definir ativo como falso e remover da cena
-		// definindo ativo para false flags nosso código de atualização para remover
-		// o marcador da matriz de balas
+		// após 500ms retira a bala da scena
 		bullet.alive = true;
 		setTimeout(function(){
 			bullet.alive = false;
 			scene.remove(bullet);
-		}, 1000);
+		}, 500);
 		
-		// add to scene, array, and set the delay to 10 frames
+		// Adiciona a bala na scena com delay de 10ms por tiro
 		bullets.push(bullet);
 		scene.add(bullet);
 		player.canShoot = 10;
@@ -198,14 +267,13 @@ function animate(){
 		camera.rotation.y - Math.PI,
 		camera.rotation.z
 	);
-	
 	renderer.render(scene, camera);
 }
 
-var cliquePressionado = false; //para controlar o tempo que o cara esta pressionando o botao do mouser
+var cliquePressionado = false; //para controlar o tempo que o cara esta pressionando o botao do mouse
 function onMouseDown (event){    cliquePressionado = true;}
 function onMouseUp (event){    cliquePressionado = false;}
-var posicaoMouser = { //controla a posiÃ§Ã£o do mouser
+var posicaoMouser = { //controla a posicao do mouse
 	x: 0,
 	y: 0
 };
@@ -218,7 +286,7 @@ var onMouseMouse = function (e){
 
 	camera.rotation.y += toRadians(deltaMovimento.x*0.1)*4;
 
-	posicaoMouser = {  //nova posiÃ§Ã£o do mouser
+	posicaoMouser = {  //nova posicao do mouser
 		x : e.offsetX,
 		y : e.offsetY
 	};
@@ -230,7 +298,7 @@ function toRadians(angle) {	return angle * (Math.PI / 180);}
 
 window.addEventListener('keydown', keyDown);
 window.addEventListener('keyup', keyUp);
-window.addEventListener('mousedown', onMouseDown ); //metodos de controle do mouser
+window.addEventListener('mousedown', onMouseDown );
 window.addEventListener('mouseup', onMouseUp ); 
 document.addEventListener('mousemove', onMouseMouse ); 
 window.onload = init;
